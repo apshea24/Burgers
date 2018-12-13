216c982ba748379b7936c5522695fdfab517cecb
shaggsworth <austin.shea89@gmail.com>
2018-12-12 21:46:13 -0700

    push

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Club Sandwich', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cuban', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pastrami', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tuna Melt', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('French Dip', FALSE);
